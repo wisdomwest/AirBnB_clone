68cf60f8578c63a9cf8db99151b0bbe656aab4ea
wisdomwest <harmonykoome@gmail.com>
2023-10-11 09:28:08 -0700

    removed redundancy

--- console.py
+++ console.py
@@ -14,53 +14,17 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = "(hbnb) "
 
-    def do_quit(self, _):
+    def do_quit(self, arg):
         """Quit command to exit the program."""
 
-        sys.exit(0)
+        return True
 
-    def do_EOF(self, _):
+    def do_EOF(self, arg):
         """End of file command to exit the program."""
 
-        sys.exit(0)
-
-
-    def do_help(self, arg):
-        """The help manual for the commands.
-
-        Args:
-            arg: Argument passed to the help command.
-        """
-
-        if arg:
-            try:
-                doc_func = getattr(self, f"help_{arg}")
-                if doc_func:
-                    doc_string = doc_func.__doc__
-                    if doc_string:
-                        print("{}".format(doc_string))
-                    else:
-                        print("No help for {}".format(arg))
-                else:
-                    print("No such command {}".format(arg))
-            except AttributeError:
-                print("No such command {}".format(arg))
-        else:
-            doc_cmds = [name[3:] for name in self.get_names() if name.startswith("do_")]
-            print("Documented commands (type help <topic>):")
-            print("========================================")
-            print(" ".join(doc_cmds))
-
-    def help_quit(self):
-        """Quit command to exit the program"""
-
-        pass
-
-    def help_EOF(self):
-        """EOF command to exit the program"""
-
-        pass
-
+        print("")
+        return True
+    
     def emptyline(self):
         """Disable last command repetition if no command is entered."""
 
