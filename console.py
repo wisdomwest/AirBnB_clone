f36ff223e910da1e99b8cfd807c0aab7eaf20e35
wisdomwest <harmonykoome@gmail.com>
2023-10-14 02:56:13 -0700

    console

--- console.py
+++ console.py
@@ -73,8 +73,8 @@ class HBNBCommand(cmd.Cmd):
             print("** no instance found **")
 
     def do_destroy(self, arg):
-         """Usage: destroy <class> <id> or <class>.destroy(<id>)
-        Delete a class instance of a id."""
+        """Usage: destroy <class> <id> or <class>.destroy(<id>)
+            Delete a class instance of a id."""
         args = arg.split()
 
         if not args:
@@ -109,6 +109,7 @@ class HBNBCommand(cmd.Cmd):
         Display string representations of all instances of a given class.
         """
         args = arg.split()
+        
         if not args:
             print([str(obj) for obj in storage.all().values()])
         elif args[0] in self.classes:
@@ -121,50 +122,59 @@ class HBNBCommand(cmd.Cmd):
     def do_update(self, arg):
         """Usage: update <class> <id> <attribute_name> <attribute_value> or
        <class>.update(<id>, <attribute_name>, <attribute_value>)
-        Update a class instance of a given id by adding or updating
-        a given attribute key/value pair or dictionary."""
-        argl = parse(arg)
-        objdict = storage.all()
+        Update a class instance of a given id"""
+        args = arg.split()
 
-        if len(argl) == 0:
+        if not args:
             print("** class name missing **")
-            return False
-        if argl[0] not in HBNBCommand.__classes:
-            print("** class doesn't exist **")
-            return False
-        if len(argl) == 1:
-            print("** instance id missing **")
-            return False
-        if "{}.{}".format(argl[0], argl[1]) not in objdict.keys():
-            print("** no instance found **")
-            return False
-        if len(argl) == 2:
-            print("** attribute name missing **")
-            return False
-        if len(argl) == 3:
-            try:
-                type(eval(argl[2])) != dict
-            except NameError:
-                print("** value missing **")
-                return False
+            return
 
-        if len(argl) == 4:
-            obj = objdict["{}.{}".format(argl[0], argl[1])]
-            if argl[2] in obj.__class__.__dict__.keys():
-                valtype = type(obj.__class__.__dict__[argl[2]])
-                obj.__dict__[argl[2]] = valtype(argl[3])
+        class_name = args[0]
+        instance_id = None
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        instance_id = args[1]
+
+        if class_name not in self.classes:
+            print("** class doesn't exist **")
+            return
+
+        cls = globals()[class_name]
+
+        if not instance_id:
+            print("** instance id missing **")
+            return
+
+        if not issubclass(cls, BaseModel):
+            print("** class doesn't exist **")
+            return
+        key = class_name + "." + instance_id
+        if key in storage.all():
+            if len(args) < 3:
+                print("** attribute name missing **")
             else:
-                obj.__dict__[argl[2]] = argl[3]
-        elif type(eval(argl[2])) == dict:
-            obj = objdict["{}.{}".format(argl[0], argl[1])]
-            for k, v in eval(argl[2]).items():
-                if (k in obj.__class__.__dict__.keys() and
-                        type(obj.__class__.__dict__[k]) in {str, int, float}):
-                    valtype = type(obj.__class__.__dict__[k])
-                    obj.__dict__[k] = valtype(v)
+                attribute_name = args[2]
+                if len(args) < 4:
+                    print("** value missing **")
                 else:
-                    obj.__dict__[k] = v
-        storage.save()
+                    attribute_value = args[3]
+                    obj = storage.all()[key]
+                    if hasattr(obj, attribute_name):
+                        if True:
+                            try:
+                                attribute_value = eval(attribute_value)
+                                setattr(obj, attribute_name, attribute_value)
+                                storage.save()
+                            except (NameError, SyntaxError):
+                                print("** invalid value for the attribute **")
+                    else:
+                        print("** attribute name is not valid or \
+                                can't be updated **")
+        else:
+             print("** no instance found **")
 
     def do_quit(self, arg):
         """Quit command to exit the program."""
